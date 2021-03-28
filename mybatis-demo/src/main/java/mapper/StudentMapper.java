704e62eebcccbd73e850f2448ac8750df2a5fd18
tianya1991 <122881060@qq.com>
2021-03-28 23:31:23 +0800

    mybatis-demo

---
+++ mybatis-demo/src/main/java/mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package mapper;
+
+import entity.StudentEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Set;
+
+public interface StudentMapper {
+
+	public StudentEntity getStudentById(int id);
+
+	public int addStudent(StudentEntity student);
+
+	public int updateStudentName(@Param("name") String name, @Param("id") int id);
+
+	public StudentEntity getStudentByIdWithClassInfo(int id);
+}
